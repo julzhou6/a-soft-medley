3036de851586598c434cef53adf7b0fb609f7b43
julzhou6 <jjkk2013@gmail.com>
2023-11-09 16:24:45 -0600

    styles added an refined rendering wordpress API content

---
+++ src/lib/api.js
@@ -0,0 +1,30 @@
+const API_URL = import.meta.env.PUBLIC_API_URL;
+
+export async function fetchAPI(query = '') {
+    console.log("api url", API_URL + "/" + query)
+    const res = await fetch(`${API_URL}/${query}`);
+
+    if (res.ok) {
+        return res.json();
+    } else {
+        const error = await res.json();
+
+        throw new Error(
+            '❗ Failed to fetch API for ' + query + "\n" +
+            'Code: ' + error.code + "\n" +
+            'Message: ' + error.message + "\n"
+        );
+    }
+}
+
+export async function getPosts() {
+    const data = await fetchAPI('posts/?per_page=20');
+
+    return data;
+}
+
+export async function getPages() {
+    const data = await fetchAPI('pages/?per_page=20');
+
+    return data;
+}
